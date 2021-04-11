5a59ff6f43299c6e0e08b2f32e30849688f59076
NishadAvnish <avnishkumar1st@gmail.com>
2021-04-11 23:22:55 +0530

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -35,8 +35,8 @@ class MyApp extends StatelessWidget {
         themeMode: ThemeMode.system,
         darkTheme: MyThemes.darkTheme,
         theme: MyThemes.lightTheme,
-        // initialRoute: counter == 0 ? "/login" : "/home",
-        initialRoute: "/secondScreen",
+        initialRoute: counter == 0 ? "/login" : "/home",
+        // initialRoute: "/secondScreen",
         onGenerateRoute: RouteGenerator.generateRoute,
       ),
     );
